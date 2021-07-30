926e445bc4cdc5291132acbffc101dd6db890d32
Gjelbrim Haskaj <g@gjelbr.im>
2021-07-30 20:37:43 +0200

    file reduction

--- users.py
+++ users.py
@@ -24,3 +24,16 @@ class Sender:
                 if status.full_text != last_tweet:
                     tweet_handler.handle(status.full_text)
                     last_tweet = status.full_text
+
+class Receiver:
+    def __init__(self):
+        try:
+            oauth = tweepy.OAuthHandler(config.RECEIVER_API_KEY,config.RECEIVER_API_SECRET_KEY)
+            oauth.set_access_token(config.RECEIVER_ACCESS_TOKEN,config.RECEIVER_ACCESS_TOKEN_SECRET)
+        except Exception:
+            print ("Receiver couldn't get authenticated")
+            sys.exit(1)
+        self.api = tweepy.API(oauth,wait_on_rate_limit=True)
+
+    def tweet(self, tweet):
+        self.api.update_status(tweet)
