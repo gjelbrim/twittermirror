e37045839d8849b33021c0d32afcb6775a9bc984
Gjelbrim Haskaj <g@gjelbr.im>
2021-07-30 18:18:54 +0200

    removed code smell

--- config.py
+++ config.py
@@ -2,20 +2,20 @@ from configparser import ConfigParser
 
 class Config:
 
-    config = ConfigParser()
-    config.read('config.ini')
+    configuration = ConfigParser()
+    configuration.read('config.ini')
     
     #keys of the sender account
-    SENDER_API_KEY = config['sender']['APIKey']
-    SENDER_API_SECRET_KEY = config['sender']['APISecretKey']
-    SENDER_ACCESS_TOKEN = config['sender']['AccessToken']
-    SENDER_ACCESS_TOKEN_SECRET = config['sender']['AccessTokenSecret']
+    SENDER_API_KEY = configuration['sender']['APIKey']
+    SENDER_API_SECRET_KEY = configuration['sender']['APISecretKey']
+    SENDER_ACCESS_TOKEN = configuration['sender']['AccessToken']
+    SENDER_ACCESS_TOKEN_SECRET = configuration['sender']['AccessTokenSecret']
 
     #keys of the receiver account
-    RECEIVER_API_KEY = config['receiver']['APIKey']
-    RECEIVER_API_SECRET_KEY = config['receiver']['APISecretKey']
-    RECEIVER_ACCESS_TOKEN = config['receiver']['AccessToken']
-    RECEIVER_ACCESS_TOKEN_SECRET = config['receiver']['AccessTokenSecret']
+    RECEIVER_API_KEY = configuration['receiver']['APIKey']
+    RECEIVER_API_SECRET_KEY = configuration['receiver']['APISecretKey']
+    RECEIVER_ACCESS_TOKEN = configuration['receiver']['AccessToken']
+    RECEIVER_ACCESS_TOKEN_SECRET = configuration['receiver']['AccessTokenSecret']
 
     #user to be mirrored
-    TWITTER_USER = config['user']['TwitterHandle']
+    TWITTER_USER = configuration['user']['TwitterHandle']
--- sender.py
+++ sender.py
@@ -4,7 +4,7 @@ import tweepy
 from handler import Handler as handler
 
 class Sender:
-    def OAuth(self):
+    def o_auth(self):
         try:
             auth = tweepy.OAuthHandler(config.SENDER_API_KEY,config.SENDER_API_SECRET_KEY)
             auth.set_access_token(config.SENDER_ACCESS_TOKEN,config.SENDER_ACCESS_TOKEN_SECRET)
@@ -14,15 +14,15 @@ class Sender:
             exit(1)
             
     def __init__(self):
-        oauth = self.OAuth()
+        oauth = self.o_auth()
         self.api = tweepy.API(oauth,wait_on_rate_limit=True)
         
     def stream(self):
-        lastTweet = ''
+        last_tweet = ''
         tweet_handler = handler()
         while True:
             for status in tweepy.Cursor(self.api.user_timeline, screen_name=config.TWITTER_USER, tweet_mode="extended").items(1):
-                if status.full_text != lastTweet:
+                if status.full_text != last_tweet:
                     tweet_handler.handle(status.full_text)
-                    lastTweet = status.full_text
+                    last_tweet = status.full_text
                     #time.sleep(120)
