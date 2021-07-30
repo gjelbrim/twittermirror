6d4d865138d833a6e74fe5817d4c40652ea0d95f
Gjelbrim Haskaj <g@gjelbr.im>
2021-07-30 20:32:25 +0200

    code reduction

--- receiver.py
+++ receiver.py
@@ -3,17 +3,13 @@ import tweepy
 from config import Config as config
 
 class Receiver:
-    def o_auth(self):
+    def __init__(self):
         try:
-            auth = tweepy.OAuthHandler(config.RECEIVER_API_KEY,config.RECEIVER_API_SECRET_KEY)
-            auth.set_access_token(config.RECEIVER_ACCESS_TOKEN,config.RECEIVER_ACCESS_TOKEN_SECRET)
-            return auth
+            oauth = tweepy.OAuthHandler(config.RECEIVER_API_KEY,config.RECEIVER_API_SECRET_KEY)
+            oauth.set_access_token(config.RECEIVER_ACCESS_TOKEN,config.RECEIVER_ACCESS_TOKEN_SECRET)
         except Exception:
             print ("Receiver couldn't get authenticated")
             sys.exit(1)
-
-    def __init__(self):
-        oauth = self.o_auth()
         self.api = tweepy.API(oauth,wait_on_rate_limit=True)
 
     def tweet(self, tweet):
